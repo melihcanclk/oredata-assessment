4d3ee131f0706722e9d343b55ba8e895cd3ae813
Melihcan Cilek <melihcanclk@hotmail.com>
2024-03-02 13:05:20 +0300

    init(backend): initialization spring boot application with postgresql database and models

---
+++ oredata_backend/src/main/java/com/cilek/cilekbank/CilekBankApplication.java
@@ -0,0 +1,12 @@
+package com.cilek.cilekbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CilekBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CilekBankApplication.class, args);
+	}
+}
---
+++ oredata_backend/src/test/java/com/cilek/cilekbank/CilekBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cilek.cilekbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CilekBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
